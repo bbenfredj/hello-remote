6ab6c16113be38f07b454cff7bedcf135c2db678
bbenfredj <bbenfredj@example.com>
2024-04-12 13:29:46 +0200

    new feature

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,17 @@
-print("Hello World!")
+
+import sys
+
+def hello_name():
+    # Check if a name is provided as a command line argument
+    if len(sys.argv) < 2:
+        print("Usage: python script_name.py <name>")
+        return
+
+    # Get the name from command line argument
+    name = sys.argv[1]
+
+    # Print the name to the console
+    print("Hello,", name)
+
+
+hello_name()
